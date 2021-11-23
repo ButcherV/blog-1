933b305728b64f64f4c4bf11bf7097fa429ea341
butcherv <butcherv@foxmail.com>
2021-11-23 09:44:27 +0800

    Post data - Asynchronous processing

--- app.js
+++ app.js
@@ -2,6 +2,35 @@ const querystring = require('querystring')
 const handleBlogRouter = require('./src/router/blog')
 const handleUserRouter = require('./src/router/user')
 
+// Processing post data
+// Mark - dont understand
+const getPostData = (req) => {
+    const promise = new Promise((resolve, reject) => {
+        if(req.method !== 'POST') {
+            resolve({})
+            return
+        }
+        if(req.headers['content-type'] !== 'application/json') {
+            resolve({})
+            return
+        }
+        let postData = ''
+        req.on('data', chunk => {
+            postData += chunk.toString()
+        })
+        req.on('end', () => {
+            if(!postData) {
+                resolve({})
+                return
+            }
+            resolve(
+                JSON.parse(postData)
+            )
+        })
+    })
+    return promise
+} 
+
 const serverHandle = (req, res) => {
     // Return JSON format
     res.setHeader('Content-type', 'application/json')
@@ -13,24 +42,28 @@ const serverHandle = (req, res) => {
     // query
     req.query = querystring.parse(url.split('?')[1])
 
-    // blog router
-    const blogData = handleBlogRouter(req, res);
-    if(blogData) {
-        res.end(JSON.stringify(blogData))
-        return
-    }
+    getPostData(req).then(postData => {
+        req.body = postData
 
-    // user router 
-    const userData = handleUserRouter(req, res);
-    if(userData) {
-        res.end(JSON.stringify(userData))
-        return
-    }
+        // blog router
+        const blogData = handleBlogRouter(req, res);
+        if(blogData) {
+            res.end(JSON.stringify(blogData))
+            return
+        }
 
-    // 404, Missed route
-    res.writeHead(404, {"Content-type": "text/plain"})
-    res.write("404 Not Fount\n")
-    res.end()
+        // user router 
+        const userData = handleUserRouter(req, res);
+        if(userData) {
+            res.end(JSON.stringify(userData))
+            return
+        }
+
+        // 404, Missed route
+        res.writeHead(404, {"Content-type": "text/plain"})
+        res.write("404 Not Fount\n")
+        res.end()
+    })
 }
 
 module.exports = serverHandle
