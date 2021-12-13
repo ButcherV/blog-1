cfeef91fa7f6d8b0f8722b3178ab69fec086135a
butcherv <butcherv@foxmail.com>
2021-12-13 08:01:17 +0800

    Session in express

--- blog-express/app.js
+++ blog-express/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -20,6 +21,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: '23Wjdw3_3x',
+  cookie: {
+    // path: '/', // default
+    // httpOnly: true, // default
+    expires: 24 * 60 * 60 * 1000,
+  }
+}))
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
--- blog-express/routes/user.js
+++ blog-express/routes/user.js
@@ -1,16 +1,78 @@
 var express = require('express');
+const { SuccessModel, ErrorModel } = require('../model/resModel')
 var router = express.Router();
+const { login } = require('../controller/user')
 
 // api: post /api/user/login
 router.post('/login', function(req, res, next) {
-    const { username, password } = req.body
-    res.json({
-        errno: 0,
-        data: {
-            username,
-            password
+    console.log('req.session is ', req.session)
+    const {username, password} = req.body
+    const result = login(username, password)
+    return result.then(data => {
+        if(data.username) {
+            // session
+            req.session.username = data.username
+            req.session.realname = data.realname
+
+            res.json(new SuccessModel())
+            return
         }
+        res.json(new ErrorModel('login fail'))  
     })
 });
+
+router.get('/login-test', (req, res, next) => {
+    console.log('req.session is ', req.session)
+    if (req.session.username) {
+        res.json({
+            errno: 0,
+            msg: '已登录'
+        })
+        return
+    }
+    res.json({
+        errno: -1,
+        msg: '未登录'
+    })
+})
+
+// router.get('/session-test', (req, res, next) => {
+//     console.log("req.session", req.session)
+//     if(session.viewNum == null) { 
+//         session.viewNum = 0
+//     }
+//     session.viewNum++
+
+//     res.json({
+//         viewNum: session.viewNum
+//     })
+// })
+
+router.get('/login-test', (req, res, next) => {
+    if(req.ression.username) {
+        res.json({
+            errno: 0,
+            msg: '已登录'
+        })
+        return
+    }
+    res.json({
+        errno: -1,
+        msg: '未登录'
+    })
+})
+
+router.get('/session-test', (req, res, next) => {
+    console.log("req.session", req.session)
+    // const session = req.session
+    // if (session.viewNum == null) {
+    //     session.viewNum = 0
+    // }
+    // session.viewNum++
+
+    res.json({
+        viewNum: req.session
+    })
+})
   
 module.exports = router;
